51a62662bcf3f83eedafb91d0b0b36f6230560f7
Ryan Clark <ryan.clark.j@gmail.com>
2012-09-13 13:00:06 -0500

    Added a Dojo-based WYSIWYG editor for TextAreas in ContentModel admin pages

--- contentmodels/admin.py
+++ contentmodels/admin.py
@@ -1,15 +1,41 @@
 from django.contrib import admin
 from models import ContentModel, ModelVersion
 
+#--------------------------------------------------------------------------------------
+# This class defines some customizations of the admin interface for ContentModels
+#--------------------------------------------------------------------------------------
 class ContentModelAdmin(admin.ModelAdmin):
+  # The Media class defines some additional media to include with an Admin page
+  #   Points to a JS and CSS file that together add a Dojo-based WYSIWYG editor
+  class Media:
+    js = [
+      'https://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojo/dojo.xd.js',
+      '/static/contentmodels/js/editor.js'
+    ]
+    css = {
+      'all': [ '/static/contentmodels/css/editor.css']
+    }    
+    
+  # Fields to display:
   list_display = ['__unicode__', 'latest_xsd_file', 'latest_xls_file']
-  search_fields = ['title']
   
-class ModelVersionAdmin(admin.ModelAdmin):
-  list_display = ['__unicode__', 'xsd_link', 'xls_link']
-  list_filter = ['content_model']
-  ordering = ['content_model__title', 'version']
-  search_fields = ['content_model__title']
-
+  # Fields on which to base the search:
+  search_fields = ['title']
 admin.site.register(ContentModel, ContentModelAdmin)
+
+#--------------------------------------------------------------------------------------
+# This class defines some customizations of the admin interface for ModelVersions  
+#--------------------------------------------------------------------------------------
+class ModelVersionAdmin(admin.ModelAdmin):
+  # Fields to display:
+  list_display = ['__unicode__', 'xsd_link', 'xls_link']
+  
+  # Fields to use in filtering on the right-hand-side:
+  list_filter = ['content_model']
+  
+  # How to order the ModelVersions:
+  ordering = ['content_model__title', 'version']
+  
+  # Fields on which to base the search:
+  search_fields = ['content_model__title']
 admin.site.register(ModelVersion, ModelVersionAdmin)
