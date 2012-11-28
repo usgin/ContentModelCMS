8d8ba2b93b81ff6d7fefc30c599f5cabcec8b39d
Ryan Clark <ryan.clark.j@gmail.com>
2012-11-28 13:46:37 -0700

    Adjust to serve static files in debug mode

--- contentmodels/urls.py
+++ contentmodels/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 urlpatterns = patterns('contentmodels.views',
 
@@ -15,3 +16,9 @@ urlpatterns = patterns('contentmodels.views',
   url(r'^models/$', 'models'),
   
 )
+
+# Serve static files
+if settings.DEBUG:
+  urlpatterns += patterns('',
+    url(r'^files/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+  )
