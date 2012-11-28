3dee20fdd7deea082cab300b663b68e9df85774f
Ryan Clark <ryan.clark.j@gmail.com>
2012-11-28 11:42:53 -0700

    Proposed change to standard urls.py

--- ContentModelCMS/urls.py
+++ ContentModelCMS/urls.py
@@ -5,7 +5,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^uri-gin/', include('uriredirect.urls')),
+    #url(r'^uri-gin/', include('uriredirect.urls')),
     url(r'^validate/', include('validation.urls')),
-    url(r'^', include('contentmodels.urls')),
+    #url(r'^', include('contentmodels.urls')),
 )
+
+from contentmodels.urls import urlpatterns as cm_urls
+urlpatterns += cm_urls
+
+from uriredirect.urls import urlpatterns as uri_urls
+urlpatterns += uri_urls
+
